363c9c12ed336f340433a1092991ccf2763bd7fc
TARRAKAN <55315542+TARRAKAN@users.noreply.github.com>
2019-10-25 20:41:08 +0300

    Add files via upload

---
+++ double.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <float.h>
+
+
+
+int main()
+
+{
+
+    double x = DBL_MAX;
+
+    printf ("At first I was like %lf,\n", x);
+
+    x *= (1 + DBL_EPSILON);
+
+    printf ("but then I was like %lf", x);
+
+    return 0;
+
+}
